9a37d0d4399fd9b06efabfeb36de0feef27299be
kevin banh <kevin.n.banh-1@ou.edu>
2020-11-04 16:50:35 -0600

    Final commit, added explanations to the output

--- src/Collect.java
+++ src/Collect.java
@@ -11,39 +11,50 @@ public class Collect {
 		// add keys and values to our hashmap
 		hash.put("Cat",1);
 		hash.put("Dog",2);
-		hash.put("Deer",3);
 		hash.put("Deer",4);
-		hash.put("Deer",5);
+		hash.put("Oyster",4);
+		hash.put("Jellyfish",4);
 		
 		// Print our hashmap
+		System.out.println("HashMap:");
 		for(Map.Entry<String, Integer> entry : hash.entrySet()) {
-			System.out.println(entry.getKey() + " " + entry.getValue());
+			System.out.print(entry.getKey() + entry.getValue() + " ");
 		}
+		System.out.println();
+		
 		
 		// Create a Hashset and add all values from hash to it
 		HashSet<String> hashUnique = new HashSet<>();
 		for(Map.Entry<String, Integer> entry2 : hash.entrySet()) {
-			hashUnique.add(entry2.getKey());
+			hashUnique.add(entry2.getValue() + " ");
 		}
 		
-		// Observe that hashUnique only has 1 deer instead of 3
+		System.out.println();
+		System.out.println("HashSet:");
 		for(String entry3 : hashUnique) {
-			System.out.println(entry3);
+			System.out.print(entry3 + " ");
 		}
+		System.out.println();
+		System.out.println("Observe that hashUnique only has 1 value of 4 instead of 3 values of 4, only counting the unique elements.");
 		
+		System.out.println();
+		System.out.println();
+		System.out.println("TreeMap:");
 		// Create a TreeMap
 		TreeMap<String,Integer> tree = new TreeMap<>();
 		// Add all values of our HashMap to our TreeMap
 		tree.putAll(hash);
-		// Observe that our TreeMap autosorts the hash set in ascending order
-		// HashMap adds each key and value, but is in a random order, not in the order it's added
-		// nor in order
+		
+		
 		for(Map.Entry<String, Integer> entry4 : tree.entrySet()) {
-			System.out.println(entry4.getKey() + " " + entry4.getValue());
+			System.out.print(entry4.getKey() + " ");
 		}
+		System.out.println();
+		System.out.println("Observe that our TreeMap autosorts the hashset in ascending alphabetical order and that the hashmap has it in a random order.");
 		
-		
-		// If we wish to order the keys and values, use TreeMap
+		System.out.println();
+		System.out.println();
+		System.out.println("Thus, it's apparent that if we want to order our values and keys, we would want to use TreeMap");
 		
 	}
 }
